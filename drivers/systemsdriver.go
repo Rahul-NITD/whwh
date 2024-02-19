e2e7d35f3d73f07b5c93974e91b36ffee67c397f
Rahul-NITD <211220042@nitdelhi.ac.in>
2024-02-20 03:26:47 +0530

    unimplemented driver

--- drivers/systemsdriver.go
+++ drivers/systemsdriver.go
@@ -1,6 +1,7 @@
 package drivers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 
@@ -11,30 +12,30 @@ type SysDriver struct{}
 
 // TesterServerStart implements specs.Tester.
 func (*SysDriver) TesterServerStart() (serverUrl string, shutdown func(), err error) {
-	panic("unimplemented")
+	return "", func() {}, fmt.Errorf("yet to implement")
 }
 
 // HookServerStart implements specs.Tester.
 func (*SysDriver) HookServerStart() (hookUrl string, outputBuffer io.Reader, shutdown func(), err error) {
-	panic("unimplemented")
+	return "", nil, func() {}, fmt.Errorf("yet to implement")
 }
 
 // HealthCheck implements specs.Tester.
 func (*SysDriver) HealthCheck(serverUrl string, hookUrl string) error {
-	panic("unimplemented")
+	return fmt.Errorf("yet to implement")
 }
 
 // ClientConnect implements specs.Tester.
 func (*SysDriver) ClientConnect(serverUrl string, hookUrl string) (client specs.ConnectedClient, sid string, err error) {
-	panic("unimplemented")
+	return nil, "", fmt.Errorf("yet to implement")
 }
 
 // ClientSubscribe implements specs.Tester.
 func (*SysDriver) ClientSubscribe(client specs.ConnectedClient, eventUrl string, sid string) (unsubscribe func(), err error) {
-	panic("unimplemented")
+	return func() {}, fmt.Errorf("yet to implement")
 }
 
 // MakeRequest implements specs.Tester.
 func (*SysDriver) MakeRequest(req *http.Request) (res *http.Response, err error) {
-	panic("unimplemented")
+	return nil, fmt.Errorf("yet to implement")
 }
