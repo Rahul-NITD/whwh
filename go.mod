81841ef93712a774f0b665e6166164fe1bdcfba2
aargeee <211220042@nitdelhi.ac.in>
2024-03-30 03:13:54 +0530

    Add SSEServer

--- go.mod
+++ go.mod
@@ -5,8 +5,14 @@ go 1.22.0
 require github.com/stretchr/testify v1.9.0
 
 require (
+	golang.org/x/net v0.0.0-20191116160921-f9c825593386 // indirect
+	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.6.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/r3labs/sse/v2 v2.10.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- whwh/server/server.go
+++ whwh/server/server.go
@@ -3,10 +3,19 @@ package server
 import (
 	"github.com/aargeee/whwh/whwh"
 	"github.com/google/uuid"
+	"github.com/r3labs/sse/v2"
 )
+
+var GlobalSSEServer = sse.New()
 
 func CreateNewChannel() whwh.CreateChannelResponse {
 	sid := uuid.NewString()
+	for GlobalSSEServer.StreamExists(sid) {
+		sid = uuid.NewString()
+	}
+
+	GlobalSSEServer.CreateStream(sid)
+
 	return whwh.CreateChannelResponse{
 		Event:    "CreateChannel",
 		Message:  "Channel Created Successfully",
